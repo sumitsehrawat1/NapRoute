d121411851c6d1f0dc275792ac379d4fa8195d27
sumitsehrawat1 <ssehrawar132@gmail.com>
2025-07-06 23:23:09 +0530

    init commit

---
+++ src/main/java/com/potodev/NapRoute/NapRouteApplication.java
@@ -0,0 +1,13 @@
+package com.potodev.NapRoute;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NapRouteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NapRouteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/potodev/NapRoute/NapRouteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.potodev.NapRoute;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NapRouteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
