89b096e2cf0f122fef54c90c59daa45c55a4ceae
sumitsehrawat1 <ssehrawar132@gmail.com>
2025-07-09 21:15:53 +0530

    improved health check

--- src/main/java/com/potodev/NapRoute/controller/HealthController.java
+++ src/main/java/com/potodev/NapRoute/controller/HealthController.java
@@ -1,5 +1,7 @@
 package com.potodev.NapRoute.controller;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,6 +10,6 @@ public class HealthController {
 
     @GetMapping("/health")
     public String healthCheck() {
-        return "OK";
+        return "OK - " + LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
     }
 }
